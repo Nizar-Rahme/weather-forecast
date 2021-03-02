f9ffd43337c3306157df52d7e0837270a42a01cf
Nizar Rahme <nizar.w.rahmah@gmail.com>
2021-03-02 06:15:02 +0200

    Add all the modules to the root module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ScheduleModule } from '@nestjs/schedule';
 
 import { configuration } from './config/configuration';
 import { validationSchema } from './config/validation';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CitiesModule } from './cities/cities.module';
+import { ForecastModule } from './forecast/forecast.module';
 import { SchedulerModule } from './scheduler/scheduler.module';
 
 @Module({
@@ -15,7 +18,18 @@ import { SchedulerModule } from './scheduler/scheduler.module';
       load: [configuration],
       validationSchema,
     }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('database.uri'),
+        useFindAndModify: false,
+        useCreateIndex: true,
+      }),
+      inject: [ConfigService],
+    }),
     ScheduleModule.forRoot(),
+    CitiesModule,
+    ForecastModule,
     SchedulerModule,
   ],
   controllers: [AppController],
